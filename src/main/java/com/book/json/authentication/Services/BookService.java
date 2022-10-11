ccc20356fe3e7c89ff009b4e3527e6a80cc5b15a
Kusuri Dheeraj Kumar <dheeraj.kusuri@modak.com>
2022-10-11 14:21:58 +0530

    Book Service changes

--- src/main/java/com/book/json/authentication/Services/BookService.java
+++ src/main/java/com/book/json/authentication/Services/BookService.java
@@ -9,10 +9,25 @@ import java.util.List;
 public class BookService {
     private static List<Books> list = new ArrayList<>();
 
-    static{
-        list.add(new Books(02,"Testing error 02"));
-        list.add(new Books(04,"Testing error 04"));
-        list.add(new Books(07,"Testing error 07"));
-
+    static {
+        list.add(new Books(02, "Testing error 02"));
+        list.add(new Books(04, "Testing error 04"));
+        list.add(new Books(07, "Testing error 07"));
     }
+
+    //GET all the books from list
+    public List<Books> getAllBooks() {
+        return list;
+    }
+
+    //GET single book by id
+    public Books getBookById(int id) {
+        Books book = null;
+
+        book = list.stream().filter(e ->
+                e.getId() == id
+        ).findFirst().get();
+        return book;
+    }
+
 }
